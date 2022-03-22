e41e135c999ff2b9a95253ca392a9dcc00abca59
colonelsalt <sondre@sondre.io>
2022-03-22 08:49:39 +0000

    Correct RandomBag implementation

--- src/RandomBag.cpp
+++ src/RandomBag.cpp
@@ -3,6 +3,7 @@
 #include <bn_fixed.h>
 #include <bn_array.h>
 #include <bn_random.h>
+#include <bn_fixed.h>
 #include "bn_sprite_items_i_tetramino.h"
 #include "bn_sprite_items_j_tetramino.h"
 #include "bn_sprite_items_l_tetramino.h"
@@ -78,6 +79,8 @@ static constexpr bn::array<t_col_grid, num_tetraminos> collision_grids =
     z_tetramino_grid
 };
 
+static bn::array<int, num_tetraminos> tetramino_index_bag = { 0, 1, 2, 3, 4, 5, 6 };
+
 const bn::sprite_item tetramino_sprite_items[num_tetraminos] =
 { 
     bn::sprite_items::i_tetramino,
@@ -89,15 +92,36 @@ const bn::sprite_item tetramino_sprite_items[num_tetraminos] =
     bn::sprite_items::z_tetramino
 };
 
-//Tetramino active_tetramino;
+constexpr int turns_between_shuffles = 7;
 constexpr bn::fixed spawn_y = (-bn::display::height() / 2) + 16;
 
 bn::random random = bn::random();
+int curr_turn = 0;
+
+void shuffle_bag(bn::array<int, num_tetraminos>& bag)
+{
+    int curr_index = num_tetraminos;
+    while (curr_index > 0)
+    {
+        int random_index = (random.get_fixed(1) * curr_index).floor_integer();
+        curr_index--;
+
+        int temp = bag[curr_index];
+        bag[curr_index] = bag[random_index];
+        bag[random_index] = temp;
+    }
+}
 
 Tetramino spawn_next()
 {
-    int index = random.get_int(num_tetraminos);
+    if (curr_turn == 0)
+    {
+        shuffle_bag(tetramino_index_bag);
+    }
+    int index = tetramino_index_bag[curr_turn];
     bn::sprite_ptr sprite = tetramino_sprite_items[index].create_sprite(0, spawn_y);
     t_col_grid collision_grid = collision_grids[index];
+    
+    curr_turn++;
     return Tetramino(sprite, collision_grid);
 }
